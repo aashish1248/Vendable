916cafc6f227de19611f574c99c6b86d5086c3b3
Aashish Chapagai <chapagaia7@gmail.com>
2024-03-06 19:07:16 +0545

    Reviews Page

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const review1 = document.getElementById("review1");
+const review2 = document.getElementById("review2");
+const review3 = document.getElementById("review3");
+const review4 = document.getElementById("review4");
+const review5 = document.getElementById("review5");
+const profile1 = document.getElementById("profile1");
+const profile2 = document.getElementById("profile2");
+const profile3 = document.getElementById("profile3");
+const profile4 = document.getElementById("profile4");
+const profile5 = document.getElementById("profile5");
+profile1.addEventListener("mouseenter", () => {
+	profile1.style.width = "5em";
+	profile1.style.height = "5em";
+})
+profile1.addEventListener("mouseleave", () => {
+	profile1.style.width = "3em";
+	profile1.style.height = "3em";
+})
+profile2.addEventListener("mouseenter", () => {
+	profile2.style.width = "5em";
+	profile2.style.height = "5em";
+})
+profile2.addEventListener("mouseleave", () => {
+	profile2.style.width = "4em";
+	profile2.style.height = "4em";
+})
+profile3.addEventListener("mouseenter", () => {
+	profile3.style.width = "5em";
+	profile3.style.height = "5em";
+})
+profile3.addEventListener("mouseleave", () => {
+	profile3.style.width = "5em";
+	profile3.style.height = "5em";
+})
+profile4.addEventListener("mouseenter", () => {
+	profile4.style.width = "5em";
+	profile4.style.height = "5em";
+})
+profile4.addEventListener("mouseleave", () => {
+	profile4.style.width = "4em";
+	profile4.style.height = "4em";
+})
+profile5.addEventListener("mouseenter", () => {
+	profile5.style.width = "5em";
+	profile5.style.height = "5em";
+})
+profile5.addEventListener("mouseleave", () => {
+	profile5.style.width = "3em";
+	profile5.style.height = "3em";
+})
